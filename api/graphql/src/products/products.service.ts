662a62ae7d014aa842741f1e2d9b25b3420e7317
Rahul <rahulsng105@gmail.com>
2022-07-29 02:09:53 +0530

    category

--- api/graphql/src/products/products.service.ts
+++ api/graphql/src/products/products.service.ts
@@ -66,6 +66,8 @@ export class ProductsService {
 
     var options_for_variations = [];
 
+    // console.log("aaaaaaaaaaaaaaaaaaaaaaaaaa");
+    // console.log(createProductInput.variation_options.upsert[0]);
     if(createProductInput.variation_options.upsert[0]?.sale_price){
       createProductInput.sale_price = createProductInput.variation_options.upsert[0].sale_price;
     }
